e4814b904f41a05ffbc9b3bdff93ee94657e9021
ailtonhs <ailtonhenrick@outlook.com>
2024-11-13 21:24:57 -0300

    Variáveis e Tipos Primitivos

---
+++ 01-exercicios/002-variaveis-e-tipos-primitivos/script.js
@@ -0,0 +1,29 @@
+let nome = "João"; //string
+let idade = 18; //number
+let salario = 1550.35; //number
+let sexo = "M"; //string
+let verdadeiro = true; //boolean
+let falso = false ; //boolean
+
+console.log(nome);
+console.log(idade);
+console.log(salario);
+console.log(sexo);
+
+let n1 = 8;
+let n2 = 5;
+
+console.log(n1 + n2);
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof verdadeiro);
+
+
+
+
+
+
+
+
+
