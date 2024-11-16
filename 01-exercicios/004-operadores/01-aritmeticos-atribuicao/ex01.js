a2d1b558a4d4bc8d0c4da3f6f45906c018a3c8d2
ailtonhs <ailtonhenrick@outlook.com>
2024-11-16 20:11:55 -0300

    Operadores

---
+++ 01-exercicios/004-operadores/01-aritmeticos-atribuicao/ex01.js
@@ -0,0 +1,53 @@
+/*
+Aritmética 
+
+5 + 2 = 7
+5 - 2 = 3
+5 * 2 = 10
+5 / 2 = 2.5
+5 % 2 = 1
+5 ** 2 = 25
+
+Ordens de precedência
+() primeiro
+ ** segundo
+ *, /, % terceiro
+ +, - último
+*/
+
+var a = 5 + 3; //8
+var b = a % 5;//3
+var c = 5 * b ** 2;//45
+var d = 10 - a / 2;//6
+var e = 6 * 2 / d;//2
+var f = b % e + 4 / e;//3
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+var n = 3;
+n += 4 //n = n + 4;
+n -= 5 //n = n - 5;
+n *= 4 //n = n * 4;
+n /= 2 //n = n / 2;
+n **= 2 //n = n ** 2;
+n %= 5 //n = n % 5;
+console.log(n);
+
+// Incremento 
+var x = 5;
+x ++; // x = x + 1;
+
+//Decremento
+x --; x = x - 1;
+
+
+
+
+
+
+
+
